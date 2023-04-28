8af413406aeef5963f2f11ec9748edd00aa4c971
rezonmain <adiazgomez101@gmail.com>
2023-04-27 19:51:33 -0700

    adds gridCreate procedure

--- apps/nextjs/src/server/api/routers/grid.ts
+++ apps/nextjs/src/server/api/routers/grid.ts
@@ -1,9 +1,19 @@
 import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
+import { create } from "~/server/inputs/grid";
 
 export const gridRouter = createTRPCRouter({
-  getAllFromUser: protectedProcedure.query(({ ctx }) => {
+  gridList: protectedProcedure.query(({ ctx }) => {
     return ctx.prisma.grid.findMany({
       where: {
+        userId: ctx.session.user.id,
+      },
+    });
+  }),
+
+  gridCreate: protectedProcedure.input(create).mutation(({ ctx, input }) => {
+    return ctx.prisma.grid.create({
+      data: {
+        ...input,
         userId: ctx.session.user.id,
       },
     });
