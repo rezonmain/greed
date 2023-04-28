2a9e3408a29bb3edae411d360da06bfc0b8f9e1d
rezonmain <adiazgomez101@gmail.com>
2023-04-27 23:00:17 -0700

    updates: gridCreate with unique name validation

--- apps/nextjs/src/server/api/routers/grid.ts
+++ apps/nextjs/src/server/api/routers/grid.ts
@@ -1,3 +1,5 @@
+import { Prisma } from "@greed/db";
+import { TRPCError } from "@trpc/server";
 import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
 import { create } from "~/server/inputs/grid";
 
@@ -10,12 +12,25 @@ export const gridRouter = createTRPCRouter({
     });
   }),
 
-  gridCreate: protectedProcedure.input(create).mutation(({ ctx, input }) => {
-    return ctx.prisma.grid.create({
-      data: {
-        ...input,
-        userId: ctx.session.user.id,
-      },
-    });
-  }),
+  gridCreate: protectedProcedure
+    .input(create)
+    .mutation(async ({ ctx, input }) => {
+      try {
+        await ctx.prisma.grid.create({
+          data: {
+            ...input,
+            userId: ctx.session.user.id,
+          },
+        });
+      } catch (error) {
+        if ((<Prisma.PrismaClientKnownRequestError>error).code === "P2002") {
+          throw new TRPCError({
+            code: "BAD_REQUEST",
+            message: "Please choose a different name for your grid.",
+            cause: error,
+          });
+        }
+        throw error;
+      }
+    }),
 });
