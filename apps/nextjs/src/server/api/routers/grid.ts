b7bdcc1b2e6aaa406a9bff27072eec7ea33e5b61
rezonmain <adiazgomez101@gmail.com>
2023-04-27 23:31:19 -0700

    adds: gridByName and gridUpdate procedure

--- apps/nextjs/src/server/api/routers/grid.ts
+++ apps/nextjs/src/server/api/routers/grid.ts
@@ -1,13 +1,24 @@
 import { Prisma } from "@greed/db";
 import { TRPCError } from "@trpc/server";
 import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
-import { create } from "~/server/inputs/grid";
+import { create, list, update } from "~/server/inputs/grid";
 
 export const gridRouter = createTRPCRouter({
-  gridList: protectedProcedure.query(({ ctx }) => {
-    return ctx.prisma.grid.findMany({
+  gridList: protectedProcedure.query(async ({ ctx }) => {
+    return await ctx.prisma.grid.findMany({
       where: {
         userId: ctx.session.user.id,
+      },
+    });
+  }),
+
+  gridByName: protectedProcedure.input(list).query(async ({ ctx, input }) => {
+    return await ctx.prisma.grid.findMany({
+      where: {
+        userId: ctx.session.user.id,
+        name: {
+          contains: input.name,
+        },
       },
     });
   }),
@@ -27,6 +38,31 @@ export const gridRouter = createTRPCRouter({
           throw new TRPCError({
             code: "BAD_REQUEST",
             message: "Please choose a different name for your grid.",
+            cause: error,
+          });
+        }
+        throw error;
+      }
+    }),
+
+  gridUpdate: protectedProcedure
+    .input(update)
+    .mutation(async ({ ctx, input }) => {
+      try {
+        await ctx.prisma.grid.update({
+          where: {
+            id: input.id,
+          },
+          data: {
+            ...input,
+            userId: ctx.session.user.id,
+          },
+        });
+      } catch (error) {
+        if ((<Prisma.PrismaClientKnownRequestError>error).code === "P2025") {
+          throw new TRPCError({
+            code: "NOT_FOUND",
+            message: "The grid you're trying to update does not exist.",
             cause: error,
           });
         }
