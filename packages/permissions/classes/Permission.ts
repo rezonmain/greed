e82697d0f88f2952953bd5765d56bbe4a3ea7a35
rezonmain <adiazgomez101@gmail.com>
2023-04-28 14:28:07 -0700

    updates: Permission clss fromRole and from static fn

--- packages/permissions/classes/Permission.ts
+++ packages/permissions/classes/Permission.ts
@@ -1,27 +1,49 @@
-import { PERM, PermissionType } from "../definition";
+import { PERM, PermissionType, RP, Roles } from "../definition";
 import z from "zod";
 
 export class Permission {
   permissions: { serialized: string; value: number };
   original: { serialized: string; value: number };
+  static PERM = PERM;
 
-  constructor(serialized: string) {
-    this.permissions = {
-      serialized,
-      value: z.number().parse(serialized),
+  constructor(input: PermissionType[] | number | string) {
+    let calculatedPermissions = {
+      serialized: "0",
+      value: 0,
     };
+
+    if (typeof input === "number") {
+      calculatedPermissions = {
+        serialized: input.toString(),
+        value: input,
+      };
+    }
+    if (typeof input === "string") {
+      calculatedPermissions = {
+        serialized: input,
+        value: z.number().parse(input),
+      };
+    } else if (Array.isArray(input)) {
+      const value = input.reduce((acc, curr) => acc | PERM[curr], 0);
+      calculatedPermissions = {
+        serialized: value.toString(),
+        value,
+      };
+    }
+    this.permissions = calculatedPermissions;
     this.original = { ...this.permissions };
+    return;
   }
 
   #setPermissions(permissions: { serialized: string; value: number }) {
     this.permissions = permissions;
   }
 
-  getValue(): number {
+  get value(): number {
     return this.permissions.value;
   }
 
-  getSerialized(): string {
+  get serialized(): string {
     return this.permissions.serialized;
   }
 
@@ -35,7 +57,19 @@ export class Permission {
     return (this.permissions.value & PERM[permission]) === PERM[permission];
   }
 
-  add(permission: PermissionType, serialized?: boolean) {
+  add(permission: PermissionType | PermissionType[], serialized?: boolean) {
+    if (Array.isArray(permission)) {
+      const newPermission = permission.reduce(
+        (acc, curr) => acc | PERM[curr],
+        this.permissions.value
+      );
+      this.#setPermissions({
+        serialized: newPermission.toString(),
+        value: newPermission,
+      });
+      return serialized ? newPermission.toString() : newPermission;
+    }
+
     const newPermission = this.permissions.value | PERM[permission];
     this.#setPermissions({
       serialized: newPermission.toString(),
@@ -56,5 +90,22 @@ export class Permission {
   reset() {
     this.#setPermissions(this.original);
     return this.original.serialized;
+  }
+
+  static from(permission: PermissionType[] | number | string) {
+    return new Permission(permission);
+  }
+
+  static fromRole(role: Roles) {
+    switch (role) {
+      case "owner":
+        return Permission.from(RP.owner);
+      case "moderator":
+        return Permission.from(RP.moderator);
+      case "user":
+        return Permission.from(RP.user);
+      default:
+        return Permission.from([]);
+    }
   }
 }
