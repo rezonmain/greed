97d6fc0c3d4bc9eab55447856e06e626fb7d1728
rezonmain <adiazgomez101@gmail.com>
2023-04-29 00:08:16 -0700

    updates: eslint rules

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -4,13 +4,14 @@ const config = {
   extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
   rules: {
     "@typescript-eslint/consistent-type-imports": [
-      "warn",
+      "error",
       {
         prefer: "type-imports",
         fixStyle: "inline-type-imports",
       },
     ],
-    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    "@next/next/no-html-link-for-pages": "off",
+    "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
   },
 };
 
