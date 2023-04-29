540211a0e68897e5c4d710cfdd7d53f4080db3a9
rezonmain <adiazgomez101@gmail.com>
2023-04-29 13:55:32 -0700

    updates: prisma client use cuid2

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,17 +1,22 @@
 import { PrismaClient, Prisma } from "@prisma/client";
+import cuid2 from "./middleware/cuid2";
 
 import { env } from "@greed/env";
+
+const createPrismaClient = (): PrismaClient => {
+  const prisma = new PrismaClient({
+    log:
+      env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+  });
+  prisma.$use(cuid2);
+  return prisma;
+};
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    log:
-      env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
-  });
+export const prisma = globalForPrisma.prisma ?? createPrismaClient();
 
 if (env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
